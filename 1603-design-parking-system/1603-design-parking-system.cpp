646ee67918511243198adf506fe9a434fdf7087a
Taranpreet Singh <taranpreetsingh15775@gmail.com>
2026-09-16 10:45:12 +0530

    Time: 11 ms (24.18%), Space: 38.7 MB (90.62%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,41 +1,19 @@
 class ParkingSystem {
 public:
-
-int b_space=0;
-int m_space=0;
-int s_space=0;
+    int spaces[3];
 
     ParkingSystem(int big, int medium, int small) {
-        b_space=big;
-        m_space=medium;
-        s_space=small;
+        spaces[0] = big;
+        spaces[1] = medium;
+        spaces[2] = small;
     }
     
     bool addCar(int carType) {
-        int x=carType;
-        if(x==1){
-            if(b_space>0){
-                b_space--;
-                return true;
-                
-            }else{
-                return false;
-            }
-        }else if(x==2){
-            if(m_space!=0){
-                m_space--;
-                return true;
-            }else{
-                return false;
-            }
-        }else if(x==3){
-            if(s_space!=0){
-                s_space--;
-                return true;
-            }else{
-                return false;
-            }
-        }return false;
+        if(spaces[carType - 1] > 0){
+            spaces[carType - 1]--;
+            return true;
+        }
+        return false;
     }
 };
 
